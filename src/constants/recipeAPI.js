df45cae0e406eb0d37174970bf187b13152085cb
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-03-31 21:00:36 +0200

    API credenciales en constants

--- src/constants/recipeAPI.js
+++ src/constants/recipeAPI.js
@@ -1,13 +1,3 @@
-// API para buscar comida
-// Doc: https://developer.edamam.com/food-database-api-docs
-// Example of URL GET from API:
-// https://api.edamam.com/api/food-database/v2/parser?app_id=2253522c&app_key=aef082754d2538e4b1e24ff04ddc4adb&ingr=50g%20rice&nutrition-type=cooking
-const SearchAPI = {
-    ID : "2253522c",
-    KEY : "aef082754d2538e4b1e24ff04ddc4adb",
-    URL : "https://api.edamam.com/api/food-database/v2/parser",
-}
-
 // API para las recertas
 // Doc: https://developer.edamam.com/edamam-docs-recipe-api
 // Example of URL GET from API:
@@ -17,3 +7,4 @@ const RecipeAPI = {
     KEY : "1f068730881ead6d9950d93dd720ab2c",
     URL : "https://api.edamam.com/api/recipes/v2",
 }
+export default RecipeAPI;
---
+++ src/constants/searchAPI.js
@@ -0,0 +1,10 @@
+// API para buscar comida
+// Doc: https://developer.edamam.com/food-database-api-docs
+// Example of URL GET from API:
+// https://api.edamam.com/api/food-database/v2/parser?app_id=2253522c&app_key=aef082754d2538e4b1e24ff04ddc4adb&ingr=50g%20rice&nutrition-type=cooking
+const SearchAPI = {
+    ID : "2253522c",
+    KEY : "aef082754d2538e4b1e24ff04ddc4adb",
+    URL : "https://api.edamam.com/api/food-database/v2/parser",
+}
+export default SearchAPI;
