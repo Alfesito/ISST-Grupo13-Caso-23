0e90d83534f74bb71f8aac1945f9f51e807c5de6
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-04-11 19:09:23 +0200

    Comentarios a SearchPage

--- src/componentes/search/SearchPage.js
+++ src/componentes/search/SearchPage.js
@@ -7,25 +7,25 @@ import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 
 function SearchPage(props) {
-  const [filtro, setFiltro] = useState("");
-  
+  const [filtro, setFiltro] = useState(""); // Estado local para almacenar el valor del filtro de búsqueda
+
   const filtrar = () => {
-    if (API.API_connection) {
-      props.onButtonClick(filtro);
+    if (API.API_connection) { // Comprobación de la conexión a la API
+      props.onButtonClick(filtro); // Si hay conexión a la API, se llama a la función onButtonClick de las props con el valor del filtro
 
     }else{
-      let nombre = filtro.toUpperCase().replace(/\s/g, "");
-      let filtrados = props.theproducts.filter((item) => {
+      let nombre = filtro.toUpperCase().replace(/\s/g, ""); // Se convierte el filtro a mayúsculas y se eliminan los espacios en blanco
+      let filtrados = props.theproducts.filter((item) => { // Se realiza el filtrado de productos basado en el filtro ingresado
         return item.food.label.toUpperCase().replace(/\s/g, "").includes(nombre);
       });
-      setFiltro(filtrados);
-      props.onInputChange(filtrados);
+      setFiltro(filtrados); // Se actualiza el estado local del filtro con los productos filtrados
+      props.onInputChange(filtrados); // Se llama a la función onInputChange de las props con los productos filtrados
     }
   };
 
-  const handleInputChange = (event) => {
-    setFiltro(event.target.value);
-    props.onInputChange(event.target.value);
+  const handleInputChange = (event) => { // Manejador del evento de cambio en el input de búsqueda
+    setFiltro(event.target.value); // Se actualiza el estado local del filtro con el valor del input
+    props.onInputChange(event.target.value); // Se llama a la función onInputChange de las props con el valor del input
   };
 
   return (
@@ -36,14 +36,14 @@ function SearchPage(props) {
             type="text"
             placeholder="Producto a buscar"
             id="filtro"
-            onChange={handleInputChange}
+            onChange={handleInputChange} // Se llama a la función handleInputChange cuando hay cambios en el input
           ></input>
 
           <Button
             id="buscador"
             variant="outline-info"
             size="sm"
-            onClick={() => filtrar()}
+            onClick={() => filtrar()} // Se llama a la función filtrar() cuando se hace clic en el botón de búsqueda
           >
             Buscar
           </Button>
@@ -56,5 +56,3 @@ function SearchPage(props) {
 }
 
 export default SearchPage;
-
-
