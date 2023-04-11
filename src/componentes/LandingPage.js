bc096b37eaded336cec2ffb1602fd9671f89ea4e
guillermo sanchez recuero <siidebox>
2023-04-11 22:43:29 +0200

    landigpage

--- src/componentes/LandingPage.js
+++ src/componentes/LandingPage.js
@@ -1,41 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
 function LandingPage() {
   return (
     <div>
-      <div class="landing-page">
-        <div class="container">
-          <div class="header-area">
-            <div class="logo">
-              Nutri<b>App</b>
-            </div>
-            <ul class="links">
-              
-              <Link to={"/login"}><li>Inicia sesión</li></Link>
-              <Link to={"/signin"}><li>Regístrate</li></Link>
-              
-            </ul>
-          </div>
-          <div class="info">
-            <h1>Página principal</h1>
-            <h2>"Donde tu nuevo tú comienza"</h2>
-
-            
-            <Link to={"/navbar"}>
-            <button>INICIO</button>
-            </Link>
-          </div>
-          <div class="image">
-            <img src="https://st2.depositphotos.com/4431055/11871/i/600/depositphotos_118710550-stock-photo-woman-eating-apple.jpg" alt=""></img>
-          </div>
-          <div class="clearfix"></div>
+      <div class="hero-image">
+        <div class="hero-text">
+          <h1 class="gradient-text active-gradient">Nutri<b>App</b></h1>
+          <p>"Donde tu nuevo tú comienza"</p>
+          <Link to={"/login"}>
+          <button class="btn-default">Iniciar sesión</button>
+          </Link>{"  "}
+          <Link to={"/signin"}>
+          <button class="btn-default">Registrarse</button>
+          </Link>
+          
         </div>
       </div>
-
-      
     </div>
   );
 }
