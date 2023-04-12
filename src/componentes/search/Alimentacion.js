3fc6d84505fc9f9daa68a77a89f6ef2d63f50dc2
Fernando Castell <fercasgdo11@gmail.com>
2023-04-12 10:39:58 +0200

    comments componente Alimentacion

--- src/componentes/search/Alimentacion.js
+++ src/componentes/search/Alimentacion.js
@@ -3,7 +3,7 @@ import SearchPage from "./SearchPage"
 import React from "react"
 
 function Alimentacion(props) {
-    return (
+    return ( // devuelve el componente de React Alimentacion
       <div>
         <Naavbar/>
         <SearchPage theproducts={props.theproducts} onInputChange={props.onInputChange} onButtonClick={props.onButtonClick} theparsed={props.theparsed}></SearchPage>
