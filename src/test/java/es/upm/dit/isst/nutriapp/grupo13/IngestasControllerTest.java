a920882001966b2393a0825fa9c0c1bb67c2817a
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-05-05 09:26:19 +0200

    tests

--- src/test/java/es/upm/dit/isst/nutriapp/grupo13/IngestasControllerTest.java
+++ src/test/java/es/upm/dit/isst/nutriapp/grupo13/IngestasControllerTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import es.upm.dit.isst.nutriapp.grupo13.controller.IngestasController;
-import es.upm.dit.isst.nutriapp.grupo13.controller.UsuariosController;
 import es.upm.dit.isst.nutriapp.grupo13.model.Ingestas;
 import es.upm.dit.isst.nutriapp.grupo13.model.Usuarios;
 import es.upm.dit.isst.nutriapp.grupo13.repository.IngestasRepository;
@@ -30,18 +32,56 @@ public class IngestasControllerTest {
     @Test
     public void testReadIngestas() throws Exception {
         // Agrega un usuario de prueba a la base de datos
-        Usuarios usuario = new Usuarios("username", "password", "email@example.com", 22, 65.0, 170, "Otro", "Femenino", "Sedentaria", "Vegetariana", "Verduras", "Ninguno");
+        Usuarios usuario = new Usuarios("username", "password", "email@example.com", 22, 65.0, 170, "Otro", "Femenino",
+                "Sedentaria", "Vegetariana", "Verduras", "Ninguno");
         usuariosRepository.save(usuario);
 
-        Ingestas ingesta = new Ingestas((long) 134, "email@example.com", LocalDate.now(), "Agua", 12.0, 12.0, 22.0, 55.0, 44.0, "A");
+        Ingestas ingesta = new Ingestas((long) 134, "email@example.com", LocalDate.now(), "Agua", 12.0, 12.0, 22.0,
+                55.0, 44.0, "A");
         ingestasRepository.save(ingesta);
 
-        Assertions.assertEquals(ingestasController.readAllIngestas("mail@example.com"), (List<Ingestas>) ingestasRepository.findAllByCorreo("mail@example.com"));
+        Assertions.assertEquals(ingestasController.readAllIngestas("mail@example.com"),
+                (List<Ingestas>) ingestasRepository.findAllByCorreo("mail@example.com"));
 
         usuariosRepository.delete(usuario);
         ingestasRepository.delete(ingesta);
+    }
 
+    @Test
+    public void testAñadirIngesta(){
+        // Agrega un usuario de prueba a la base de datos
+        Usuarios usuario = new Usuarios("username", "password", "email@example.com", 22, 65.0, 170, "Otro", "Femenino",
+                "Sedentaria", "Vegetariana", "Verduras", "Ninguno");
+        usuariosRepository.save(usuario);
+
+        Ingestas ingesta = new Ingestas((long) 134, "email@example.com", LocalDate.now(), "Agua", 12.0, 12.0, 22.0,
+                55.0, 44.0, "A");
+        ingestasRepository.save(ingesta);
+
+        // Comprobar que la respuesta es 200 OK
+        Assertions.assertEquals(ingestasController.añadirIngesta(ingesta), ResponseEntity.ok("Alimento registrado exitosamente."));
+
+        // Comprobar que el mensaje de la respuesta es correcto
+        Assertions.assertEquals(ingestasController.añadirIngesta(null), ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al registrar el alimento."));
+
+        usuariosRepository.delete(usuario);
+        ingestasRepository.delete(ingesta);
+    }
+
+    @Test
+    public void testEliminarIngesta() {
+        Ingestas ingesta = new Ingestas((long) 134, "email@example.com", LocalDate.now(), "Agua", 12.0, 12.0, 22.0,
+                55.0, 44.0, "A");
+        ingestasRepository.save(ingesta);
+
+        Assertions.assertNotNull(ingesta.getId());
+
+        // Assertions.assertTrue(ingestasRepository.existsById(ingesta.getId()));
+
+        ingestasRepository.delete(ingesta);
+
+        // Comprobar que la ingesta se ha eliminado correctamente
+        Assertions.assertFalse(ingestasRepository.existsById(ingesta.getId()));
     }
 
 }
-
